4ff3915249c1db66e9e437a857a21bb60e7d23b3
Sailesh1993 <karki.saileshnep@gmail.com>
2024-08-22 11:24:23 +0300

    Second commit, interactive demo is created.

--- variableDemo.py
+++ variableDemo.py
@@ -1,6 +1,8 @@
+
 intVariable = 4
 floatVariable = 4.5
 stringVariable = "Software1 is fun"
+
 
 print(intVariable)
 print(floatVariable)
@@ -8,3 +10,26 @@ print(stringVariable)
 print (type(intVariable))
 print (type(floatVariable))
 print (type(stringVariable))
+
+
+#typecasting is fun
+
+intVariable = int(floatVariable)
+print("Here is the int version of float variable",intVariable)
+
+shareOfLoan = 500.50/3
+print(shareOfLoan)
+print(int(shareOfLoan))
+print(type(shareOfLoan))
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+sum = num1 + num2
+print(sum)
+
+
+name = input("Enter your name: ")
+school = input("Enter your school: ")
+print("you are", name, " and your school name is", school)
+print(f"you name is, {name}, and your school name is {school}")
+
