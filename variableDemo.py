8f2f72ec826f733e05d316637cd59c6535bd8eb5
Sailesh1993 <karki.saileshnep@gmail.com>
2024-08-22 10:38:11 +0300

    initial commit, variables are defined.

---
+++ variableDemo.py
@@ -0,0 +1,10 @@
+intVariable = 4
+floatVariable = 4.5
+stringVariable = "Software1 is fun"
+
+print(intVariable)
+print(floatVariable)
+print(stringVariable)
+print (type(intVariable))
+print (type(floatVariable))
+print (type(stringVariable))
